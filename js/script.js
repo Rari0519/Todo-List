24af2a54ba480feebb55298c3c5862ea2d7e6401
Rayane Ferreira <ferreirarayane2002@gmail.com>
2024-08-23 15:31:21 -0300

    refiz a função de adicionar mais organizada

--- js/script.js
+++ js/script.js
@@ -1,37 +1,23 @@
-
 const input = document.querySelector('#input');
 const btnAdd = document.querySelector('.add');
-const tarefas = document.querySelector('.lista-tarefas');
+const ul = document.querySelector('.lista-tarefas');
 
-let listaDeTarefas = [];
-
-//função de pegar o valor do input
-function adicionaTarefa(){
-    listaDeTarefas.push(input.value); //push adiciona item na array 
-    mostraTarefaTela();
-}
-function mostraTarefaTela(){
-    let novaTarefa = ''
-
-    listaDeTarefas.forEach((tafera) => {
-        //tarefa que ja estava mais a nova 
-        novaTarefa = novaTarefa + `
-        <li>
-        <span>${tafera}</span>
+function adicionaTarefa() {
+    const tarefa = input.value.trim(); //remove espaço em branco
+    
+    if (tarefa !== "") { 
+        const li = document.createElement('li'); //li dinamico
+        li.innerHTML = `
+            <span>${tarefa}</span>
             <div class="tarefas">
                 <button class="concluido">✓</button>
                 <button class="editar"><i class='bx bxs-edit'></i></button>
                 <button class="deletar">✗</button>
             </div>
-        </li>
-        `
-    });
-
-    tarefas.innerHTML = novaTarefa //inserindo a tarefa visualmente no html
+        `;
+        ul.appendChild(li); 
+        input.value = ""; 
+    }
 }
 
-
-
-//eventos de click
 btnAdd.addEventListener("click", adicionaTarefa);
-
