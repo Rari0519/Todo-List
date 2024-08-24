6af8a9b1f713e7056a8331fb2d3e7fd0379b884b
muriloifms <murilo.oliveira4@estudante.ifms.edu.br>
2024-08-24 15:30:33 -0400

    [Adicionando funcionalidade de deletar, concluir, editar, filtro na nova tela Bootstrap]

--- js/script.js
+++ js/script.js
@@ -22,48 +22,57 @@ function construir_div(tarefas) {
     }
     // funcao para construir o HTML interno da tarefa
     function construir_tarefa() {
-        
+
         function construir_container_tarefa() {
             const tarefa = document.createElement('li')
-            tarefa.classList.add('tarefa')
+            tarefa.className = 'list-group-item d-flex align-items-center tarefa'
             return tarefa
         }
 
         function construir_nome(valor, obj) {
-            const nome = document.createElement('div')
-            nome.textContent = valor
-            nome.classList.add('nome')
-            if (tarefas[obj].Concluido == true) { nome.classList.add('Concluido')}
-            return nome
-        }
 
+            const wrapper = document.createElement('div')
+            wrapper.className = 'form-check'
+
+            const input = document.createElement('input')
+            input.setAttribute('type', 'checkbox')
+            input.className = 'form-check-input me-2 Concluir CRUD'
+            input.id = `flexCheckDefault concluir#${obj}`
+
+            const nome = document.createElement('label')
+            nome.textContent = valor
+            nome.className = 'form-check-label'
+
+            wrapper.append(input, nome)
+
+            if (tarefas[obj].Concluido == true) { input.checked = true}
+            return wrapper
+        }
+        /*
         function construir_prioridade(valor) {
             const prioridade = document.createElement('div')
             prioridade.textContent = valor
             prioridade.classList.add('prioridade')
             return prioridade
         }
+        */
 
         function construir_botoes(obj_index) {
             const botoes = document.createElement('div')
-            botoes.classList.add('botoes')
+            botoes.className = "ms-auto p-1"
 
-            const concluir = document.createElement('button')
             const excluir = document.createElement('button')
             const editar = document.createElement('button')
-            
-            concluir.className = 'Concluir CRUD'
-            excluir.className = 'Excluir CRUD'
-            editar.className = 'Editar CRUD'
 
-            concluir.id = `concluir#${obj_index}`
+            excluir.className = 'btn btn-danger btn-sm Excluir CRUD'
+            editar.className = 'btn btn-primary btn-sm me-2  Editar CRUD'
+
             excluir.id = `excluir#${obj_index}`
             editar.id = `editar#${obj_index}`
 
-            concluir.innerHTML = "<i class='bx bx-check-circle' ></i>"
             editar.innerHTML = "<i class='bx bxs-pencil' ></i>"
             excluir.innerHTML = "<i class='bx bx-x-circle'></i>"
-            botoes.append(concluir, editar, excluir)
+            botoes.append(editar, excluir)
             return botoes
         }
 
@@ -71,15 +80,15 @@ function construir_div(tarefas) {
         for (obj in tarefas) {
             const tarefa = construir_container_tarefa()
             const nome = construir_nome(tarefas[obj].Nome, obj)
-            const prioridade = construir_prioridade(tarefas[obj].Prioridade)
+            //const prioridade = construir_prioridade(tarefas[obj].Prioridade)
             const botoes = construir_botoes(obj)
-            tarefa.append(nome, prioridade, botoes)
+            tarefa.append(nome, botoes)
             container.append(tarefa)
         }
 
     }
 
-    const container = document.querySelector('.lista-tarefas')
+    const container = document.querySelector('.list-group')
     limpar_div(container)
 
 
@@ -92,9 +101,8 @@ function adicionar_eventos_botoes() {
 
     const buttons = document.querySelectorAll('.CRUD')
     const tarefa = document.querySelectorAll('.tarefa')
-
     // todo botão é criado com um ID. esse id equivale a sua tarefa "pai"
-        // exemplo: se eu clicar no botão X da primeira tarefa da lista, o indice retornado é 0 e assim em diante.
+    // exemplo: se eu clicar no botão X da primeira tarefa da lista, o indice retornado é 0 e assim em diante.
 
     // a funcao abaixo serve justamente para isso, pegar o indice do botão clicado
     function encontrar_indice(elemento) {
@@ -102,12 +110,12 @@ function adicionar_eventos_botoes() {
         const valor_do_ID = elemento.slice(indice_apos_HASH + 1).trim()
         return valor_do_ID
     }
-    
+
 
     function pegar_nome_task(elemento) {
         return elemento.firstElementChild
     }
-    
+
     // funcao para adicionar o evento de editar
     function adicionar_editar(nome, array) {
         // funcao cria uma tag form e de input
@@ -140,17 +148,17 @@ function adicionar_eventos_botoes() {
     }
 
     function evento_remover(array, indice) {
-        array.splice(indice , 1)
+        array.splice(indice, 1)
         construir_div(array)
     }
 
     function adicionar_concluir(elemento, array) {
-        elemento.classList.toggle('Concluido')
+        elemento.classList.add('text-decoration-line-through')
         array.Concluido = true
     }
 
     function desfazer_concluir(elemento, array) {
-        elemento.classList.toggle('Concluido')
+        elemento.classList.remove('text-decoration-line-through')
         array.Concluido = false
     }
 
@@ -166,7 +174,7 @@ function adicionar_eventos_botoes() {
             }
 
             if (button.id.includes('concluir')) {
-                const nome_task = pegar_nome_task(tarefa[index])
+                const nome_task = tarefa[index].firstElementChild.childNodes[1]
                 if (tarefas[index].Concluido == false) {
                     adicionar_concluir(nome_task, tarefas[index])
                 }
@@ -181,16 +189,37 @@ function adicionar_eventos_botoes() {
 
 // adiciona o filtro, ainda precisa de teste e correçoes
 function adicionar_filtro() {
-    const select = document.querySelector('select')
-    select.addEventListener('input', (e) => {
-        const tarefas_filtradas = []
-        for (obj in tarefas) {
-            if (tarefas[obj].Prioridade == select.value) {
-                tarefas_filtradas.push(tarefas[obj])
+    const buttons = document.querySelectorAll(".btn-primary")
+    const tarefas_pendentes = []
+    const tarefas_concluidas = []
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            if (button.id == 'todas') {
+                construir_div(tarefas)
             }
-        }
-        construir_div(tarefas_filtradas)
+
+            else if (button.id == 'concluidas') {
+                for (obj in tarefas) {
+                    if (tarefas[obj].Concluido == true) {
+                        tarefas_concluidas.push(tarefas[obj])
+                    }
+                }
+                construir_div(tarefas_concluidas)
+            }
+
+            else if (button.id == 'pendentes') {
+                for (obj in tarefas) {
+                    if (tarefas[obj].Concluido == false) {
+                        tarefas_pendentes.push(tarefas[obj])
+                    }
+                }
+                construir_div(tarefas_pendentes)
+            }
+        })
     })
+
+
 }
 
 construir_div(tarefas)
