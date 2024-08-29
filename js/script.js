857813be3c53af4c0601855fd7058ac3ba856e81
Rayane Ferreira <ferreirarayane2002@gmail.com>
2024-08-29 08:08:53 -0300

    Revert "refiz a função de adicionar mais organizada"
    This reverts commit 24af2a54ba480feebb55298c3c5862ea2d7e6401.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const input = document.querySelector('#tarefa-input');
 const btnAdd = document.querySelector('#btn-add');
 const tarefas = document.querySelector('.tarefas');
@@ -58,3 +59,42 @@ function mostrarNaTela() {
 
 
 btnAdd.addEventListener("click", adicionaTarefa);
+=======
+
+const input = document.querySelector('#input');
+const btnAdd = document.querySelector('.add');
+const tarefas = document.querySelector('.lista-tarefas');
+
+let listaDeTarefas = [];
+
+//função de pegar o valor do input
+function adicionaTarefa(){
+    listaDeTarefas.push(input.value); //push adiciona item na array 
+    mostraTarefaTela();
+}
+function mostraTarefaTela(){
+    let novaTarefa = ''
+
+    listaDeTarefas.forEach((tafera) => {
+        //tarefa que ja estava mais a nova 
+        novaTarefa = novaTarefa + `
+        <li>
+        <span>${tafera}</span>
+            <div class="tarefas">
+                <button class="concluido">✓</button>
+                <button class="editar"><i class='bx bxs-edit'></i></button>
+                <button class="deletar">✗</button>
+            </div>
+        </li>
+        `
+    });
+
+    tarefas.innerHTML = novaTarefa //inserindo a tarefa visualmente no html
+}
+
+
+
+//eventos de click
+btnAdd.addEventListener("click", adicionaTarefa);
+
+>>>>>>> parent of 24af2a5 (refiz a função de adicionar mais organizada)
