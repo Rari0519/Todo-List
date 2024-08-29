f804f4895ff014da8d6e8c05198a90f500d3e300
Rayane Ferreira <ferreirarayane2002@gmail.com>
2024-08-29 07:42:42 -0300

    refiz crud js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,60 @@
 const input = document.querySelector('#tarefa-input');
 const btnAdd = document.querySelector('#btn-add');
-listaTarefas = []
+const tarefas = document.querySelector('.tarefas');
+const btnTodos = document.querySelector('.btn-todas');
+const btnConcluidas = document.querySelector('.btn-concluidas');
+const btnPendentes = document.querySelector('.btn-pendentes')
+let listaTarefas = [];
 
-//Função para carregar tarefas do localStorage
-function adicionaNoStorage(){
-
-}
-//função adicionar tarefa
-function adicionaTarefa(event){
-    event.preventDefault(); // Impede o comportamento padrão do navegador.
-    let valorInput = input.value.trim();
-    if(valorInput !== ""){
-        listaTarefas.push(valorInput);
-        input.value = ''
+function adicionaTarefa() {
+    const tarefa = input.value.trim();
+    if (tarefa !== "") {
+        listaTarefas.push({
+            tarefa: tarefa,
+            concluida: false
+        });
+        mostrarNaTela();
     }
-    console.log(listaTarefas)
-};
-btnAdd.addEventListener("click",adicionaTarefa);
+    input.value = '';
+}
+
+function deletar(posicao) {
+    listaTarefas.splice(posicao, 1);
+    mostrarNaTela();
+}
+
+function editar(posicao) {
+    const novaTarefa = prompt("Edite a tarefa...");
+    if (novaTarefa !== null && novaTarefa.trim() !== "") {
+        listaTarefas[posicao].tarefa = novaTarefa.trim();
+        mostrarNaTela();
+    }
+}
+
+function concluir(posicao) {
+}
+
+function mostrarNaTela() {
+    let li = "";
+
+    listaTarefas.forEach((itemTarefa, posicao) => {
+        const classeFeito = itemTarefa.concluida ? 'feito' : '';
+        
+        li += `
+       <li class="list-group-item d-flex justify-content-between align-items-center ${classeFeito}">
+        <div class="form-check">
+            <label class="form-check-label" for="input">${itemTarefa.tarefa}</label>
+        </div>
+        <div class="ms-auto p-1">
+            <button type="button" class="btn btn-success btn-sm" onclick="concluir(${posicao})"><i class='bx bx-check'></i></button>
+            <button type="button" class="btn btn-primary btn-sm me-2" onclick="editar(${posicao})"><i class='bx bxs-pencil'></i></button>
+            <button type="button" class="btn btn-danger btn-sm" onclick="deletar(${posicao})"><i class='bx bx-x'></i></button>
+        </div>
+        </li>`;
+    });
+
+    tarefas.innerHTML = li;
+}
+
+
+btnAdd.addEventListener("click", adicionaTarefa);
