ace017d31b81e1891b8480d16549f8ff2c5f7656
Rari0519 <rarianed.s.p0519@gmail.com>
2024-09-01 10:45:13 -0400

    revisado funções adicionar e exibir tasks

--- js/script.js
+++ js/script.js
@@ -1,91 +1,100 @@
+// Selecionando elementos
+const input = document.querySelector("#tarefa-input"); /* input */
+const btnAdd = document.querySelector("#btn-add"); /* Botão add */
+const ulTarefas = document.querySelector(".list-task"); /* Lista tarefas- */
+// const tarefas = document.querySelector('.tarefas');
+const btn_filtros = document.querySelectorAll(".filtro");
 
+// ------------------------------------------------------------------------------
 
-const input = document.querySelector('#tarefa-input');
-const btnAdd = document.querySelector('#btn-add');
-const tarefas = document.querySelector('.tarefas');
-const btn_filtros = document.querySelectorAll('.filtro')
-
+// definindo array para armazenar as lista de tarefas
 let listaTarefas = [];
 
+// Funções
 function adicionaTarefa() {
-    const tarefa = input.value.trim();
-    if (tarefa !== "") {
-        listaTarefas.push({
-            tarefa: tarefa,
-            concluida: false
-        });
-        mostrarNaTela(listaTarefas);
-    }
-    input.value = '';
+  const tarefa = input.value.trim();
+  if (tarefa !== "") {
+    listaTarefas.push(input.value);
+    mostrarNaTela(listaTarefas);
+  }
+  input.value = ""; /* limpando input após adicionar nova tarefa */ 
 }
 
-function deletar(posicao) {
-    listaTarefas.splice(posicao, 1);
-    mostrarNaTela();
+function mostrarNaTela() {
+  let newLi = "";
+
+  listaTarefas.forEach((itemTarefa) => {
+    newLi =
+      newLi +
+      `
+            <li class="tarefas">
+                <h3>${itemTarefa}</h3>
+                <div class="list-priority"></div>
+                <button type="button" class="btn btn-outline-danger">
+                    <i class="bi bi-check-lg"></i>
+                </button>
+                <button type="button" class="btn btn-outline-danger">
+                    <i class="bi bi-pencil-fill"></i>
+                </button>
+                <button type="button" class="btn btn-outline-danger">
+                    <i class="bi bi-x-lg"></i>
+                </button>
+            <li/>
+        `;
+  });
+    ulTarefas.innerHTML = newLi;
+    // console.log(JSON.stringify(mostrarNaTela));
 }
 
-function editar(posicao) {
-    const novaTarefa = prompt("Edite a tarefa...");
-    if (novaTarefa !== null && novaTarefa.trim() !== "") {
-        listaTarefas[posicao].tarefa = novaTarefa.trim();
-        mostrarNaTela();
-    }
-}
+// function deletar(posicao) {
+//     listaTarefas.splice(posicao, 1);
+//     mostrarNaTela();
+// }
 
-function concluir(posicao) {
-    listaTarefas[posicao].concluida = !listaTarefas[posicao].concluida;
+// function editar(posicao) {
+//     const novaTarefa = prompt("Edite a tarefa...");
+//     if (novaTarefa !== null && novaTarefa.trim() !== "") {
+//         listaTarefas[posicao].tarefa = novaTarefa.trim();
+//         mostrarNaTela();
+//     }
+// }
 
-    const label = document.querySelector(`#label-${posicao}`);
+// function concluir(posicao) {
+//     listaTarefas[posicao].concluida = !listaTarefas[posicao].concluida;
 
-    if (listaTarefas[posicao].concluida == true) {
-        label.style.textDecoration = 'line-through';
-    } else{
-        label.style.textDecoration = 'none';
-    }
-}
+//     const label = document.querySelector(`#label-${posicao}`);
 
-function mostrarNaTela(listaTarefas) {
-    let li = "";
-    listaTarefas.forEach((itemTarefa, posicao) => {
-        li = li + `
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-         <div class="form-check">
-             <input class="form-check-input" type="checkbox" onchange="concluir(${posicao})">
-             <label class="form-check-label" id="label-${posicao}" for="input">${itemTarefa.tarefa}</label>
-         </div>
-         <div class="ms-auto p-1">
-             <button type="button" class="btn btn-primary btn-sm me-2" onclick="editar(${posicao})"><i class='bx bxs-pencil'></i></button>
-             <button type="button" class="btn btn-danger btn-sm" onclick="deletar(${posicao})"><i class='bx bx-x'></i></button>
-         </div>
-         </li>`;
-    });
+//     if (listaTarefas[posicao].concluida == true) {
+//         label.style.textDecoration = 'line-through';
+//     } else{
+//         label.style.textDecoration = 'none';
+//     }
+// }
 
-    tarefas.innerHTML = li;
-}
+// // utiliza o nome da classe e o atributo concluido para gerar um novo array com as tarefas filtradas
+// function filtrarTarefas(btn, tarefas) {
 
-// utiliza o nome da classe e o atributo concluido para gerar um novo array com as tarefas filtradas
-function filtrarTarefas(btn, tarefas) {
+//     const tarefas_filtradas = []
 
-    const tarefas_filtradas = []
+//    for (obj in tarefas) {
+//         if (btn.className.includes('pendentes') && tarefas[obj].concluida == false) {
+//             tarefas_filtradas.push(tarefas[obj])
+//         }
+//         else if (btn.className.includes('concluidas') && tarefas[obj].concluida == true) {
+//             tarefas_filtradas.push(tarefas[obj])
+//         }
+//         else if (btn.className.includes('todas')) {
+//             mostrarNaTela(listaTarefas)
+//             return
+//         }
+//     }
 
-   for (obj in tarefas) {
-        if (btn.className.includes('pendentes') && tarefas[obj].concluida == false) {
-            tarefas_filtradas.push(tarefas[obj])
-        }
-        else if (btn.className.includes('concluidas') && tarefas[obj].concluida == true) {
-            tarefas_filtradas.push(tarefas[obj])
-        }
-        else if (btn.className.includes('todas')) {
-            mostrarNaTela(listaTarefas)
-            return
-        }
-    }
-    
-    mostrarNaTela(tarefas_filtradas)
+//     mostrarNaTela(tarefas_filtradas)
 
-}
+// }
 
+// ------------------------------------------------------------------------------
+
+// Eventos
 btnAdd.addEventListener("click", adicionaTarefa);
-btn_filtros.forEach((btn) => btn.addEventListener("click", () => { filtrarTarefas(btn, listaTarefas) }));
-
-
+// btn_filtros.forEach((btn) => btn.addEventListener("click", () => { filtrarTarefas(btn, listaTarefas) }));
